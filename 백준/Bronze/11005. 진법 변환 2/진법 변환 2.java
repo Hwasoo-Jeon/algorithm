d100ceaad71862dad3c0366e9a95ed80310e8e4a
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2023-10-23 18:29:52 +0900

    [Bronze I] Title: 진법 변환 2, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num1 = Integer.parseInt(st.nextToken());
+		int num2 = Integer.parseInt(st.nextToken());
+		bw.write((Integer.toString(num1,num2)).toUpperCase());
+		bw.flush();
+	}
+}
