4b82881edc2cc63f00ace64db1aaa282aab22305
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-03-15 17:28:07 +0900

    [Silver I] Title: 안전 영역, Time: 368 ms, Memory: 56108 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,88 +1,65 @@
 import java.util.*;
 import java.io.*;
 
-class Coord {
-	int y;
-	int x;
-	
-	public Coord(int y, int x) {
-		this.y = y;
-		this.x = x;
-	}
-}
-
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st; 
-		int size = Integer.parseInt(br.readLine());
-		int[][] info = new int[size][size];
-		int[][] map = new int[size][size];
-		LinkedHashSet<Integer> height = new LinkedHashSet<>();
-		
-		int[] move_y = {-1,1,0,0};
-		int[] move_x = {0,0,-1,1};
-		
-		for(int i=0; i< size; i++) {//100*100
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j< size; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				info[i][j] = num;
-				height.add(num);
-			}
-		}
-		
-		height.add(0);
-		int result = 0;
-		
-		for(int num : height) { //100
-			int count = 0;
-			Queue<Coord> queue = new LinkedList<>();
-			boolean[][] visited = new boolean[size][size];
 	
-			//이동 가능 영역 표기
-			for(int i=0; i<size; i++) { //100*100
-				for(int j=0; j<size; j++) {
-					if(info[i][j] <= num) {
-						map[i][j] = 0;
-					}else {
-						map[i][j] = 1;
-					}
-				}
-			}
-			
-			//while
-			for(int i=0; i<size; i++) { //100*100
-				for(int j=0; j<size; j++) {
-					if(map[i][j]==1 && !visited[i][j]) {
-						queue.offer(new Coord(i,j));
-						visited[i][j] = true;
-						
-						while(!queue.isEmpty()) {
-							Coord cur = queue.poll();
-							for(int dir = 0; dir<4; dir++) {
-								int newY = cur.y + move_y[dir];
-								int newX = cur.x + move_x[dir];
-								
-								if(newY>=0&& newY<size&& newX>=0 && newX<size) {
-									if(map[newY][newX]==1 &&!visited[newY][newX]) {
-										queue.offer(new Coord(newY,newX));
-										visited[newY][newX] = true;
-									}
-								}
-							}
-						}
-						count++;
-					}
-				}
-			}	
-			//result 수정
-			if(count >result) {
-				result = count;
+	static int curAnswer = 0;
+	static boolean[][] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[][] map = new int[n][n];
+		
+		//층수 set
+		Set<Integer> level = new HashSet<>();
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				int cur = Integer.parseInt(st.nextToken());
+				map[i][j] = cur;
+				level.add(cur);
 			}
 		}
-		bw.write(result+"");
-		bw.flush();
+		
+		int answer = 1;
+		for(int depth : level) {
+			curAnswer = 0;
+			visited = new boolean[n][n];
+			for(int r=0; r <n; r++) {
+				for(int c=0; c<n; c++) {
+					if(map[r][c] > depth && !visited[r][c]) {
+						bfs(map, r, c, n, depth);
+						
+					}
+				}
+			}
+			if(curAnswer > 0) answer = Math.max(answer, curAnswer);
+		}
+		
+		System.out.println(answer+"");
+	}
+	
+	private static void bfs(int[][] map, int r, int c, int n, int depth) {
+		visited[r][c] = true;
+		int[] moveY = {-1,1,0,0};
+		int[] moveX = {0,0,-1,1};
+		Queue<Integer[]> q = new LinkedList<>();
+		q.offer(new Integer[] {r,c});
+		while(!q.isEmpty()) {
+			Integer[] cur = q.poll();
+			for(int i = 0; i<4; i++) {
+				int curY = cur[0]+moveY[i];
+				int curX = cur[1]+moveX[i];
+				if(curY>=0 && curY <n && curX>=0 && curX<n) {
+					if(!visited[curY][curX] && map[curY][curX] > depth) {
+						visited[curY][curX] = true;
+						q.offer(new Integer[] {curY,curX});
+					}
+				}
+			}
+		}
+		curAnswer ++;
 	}
 }
