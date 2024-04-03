8a5353a13d023a2d0bc7c374a3f7d683a368eb1c
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-04-03 17:21:50 +0900

    [Silver I] Title: 회전 초밥, Time: 184 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int[] plate;
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,55 +10,89 @@ public class Main {
 		int d = Integer.parseInt(st.nextToken()); //3000
 		int K = Integer.parseInt(st.nextToken()); //3000
 		int c = Integer.parseInt(st.nextToken()); //3000
-		plate = new int[N];
-	
+		int[] plate = new int[N];
+		int[] eating = new int[d+1];
+		
 		for(int i=0; i<N; i++) {
 			plate[i] = Integer.parseInt(br.readLine());
 		}
-
-		//회전초밥모양 만들기
-		ArrayList<Integer> list = new ArrayList<>();
-		for(int i=0; i<N; i++) {
-			list.add(plate[i]);
-		}
-		for(int i=0; i<K-1; i++) {
-			list.add(plate[i]);
-		}
 		
-		//투포인터 list,set 생성
-		ArrayList<Integer> twoList = new ArrayList<>();
-		HashSet<Integer> set = new HashSet<>();
-		int left = 0;
-		int right = K-1;
+		//투포인터 사용
 		int answer = 0;
-		for(int i=0; i<=K-1; i++) {
-			twoList.add(plate[i]);
-			set.add(plate[i]);
-		}
+		eating[c]=1;
 		
-		while(true) {
-			if(set.contains(c)) {
-				answer= Math.max(answer, set.size());
-			} else {
-				answer= Math.max(answer, set.size()+ 1); 
+		int cnt = 1;
+		for(int i=0; i<K;i++) {
+			if(eating[plate[i]]==0) {
+				cnt++;
 			}
+			eating[plate[i]]++;
+		}
+		answer = cnt;
+
+		for(int i=1; i<N; i++) {
+			int right = (i+K-1)%N; //i
+			if(eating[plate[i-1]]==1) {
+				cnt--;
+			}
+			eating[plate[i-1]]--;
+			if(eating[plate[right]]==0) {
+				cnt++;
+			}
+			eating[plate[right]]++;
 			
-			left++;
-			right++; //left 0 right 3 list size 11
-			twoList.remove(list.get(left-1));
-			twoList.add(list.get(right));
-			set = new HashSet<>(twoList);
-			if(right == list.size()-1) {
-				break;
-			}
+			answer = Math.max(answer, cnt);
 		}
 		
-		if(set.contains(c)) {
-			answer= Math.max(answer, set.size());
-		} else {
-			answer= Math.max(answer, set.size()+ 1); 
-		}
 		System.out.println(answer);
 	}
+
 }
-// 참고사항 : 투포인터 활용, poll이 필요하지 않다면, list를 추가로 연결하여 위와 같이 활용 가능.
+
+/*
+// right list,twoList를 삭제하고 바로 set에 담으면 시간절약됨.
+//회전초밥모양 만들기
+ArrayList<Integer> list = new ArrayList<>();
+for(int i=0; i<N; i++) {
+	list.add(plate[i]);
+}
+for(int i=0; i<K-1; i++) {
+	list.add(plate[i]);
+}
+
+//투포인터 list,set 생성
+ArrayList<Integer> twoList = new ArrayList<>();
+HashSet<Integer> set = new HashSet<>();
+int left = 0;
+int right = K-1;
+int answer = 0;
+for(int i=0; i<=K-1; i++) {
+	twoList.add(plate[i]);
+	set.add(plate[i]);
+}
+
+while(true) {
+	if(set.contains(c)) {
+		answer= Math.max(answer, set.size());
+	} else {
+		answer= Math.max(answer, set.size()+ 1); 
+	}
+	
+	left++;
+	right++; //left 0 right 3 list size 11
+	twoList.remove(list.get(left-1));
+	twoList.add(list.get(right));
+	set = new HashSet<>(twoList);
+	if(right == list.size()-1) {
+		break;
+	}
+}
+
+if(set.contains(c)) {
+	answer= Math.max(answer, set.size());
+} else {
+	answer= Math.max(answer, set.size()+ 1); 
+}
+
+System.out.println(answer);
+*/
