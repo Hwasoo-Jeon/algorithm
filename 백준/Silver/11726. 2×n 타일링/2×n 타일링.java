1f4c4b0133e146a9353679815c4a402d9f64e143
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-04-09 13:23:05 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,24 +1,20 @@
 import java.io.*;
-
+import java.util.*;
 public class Main {
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int size = Integer.parseInt(br.readLine());
-
-		if (size == 1 || size == 2) {
-			bw.write(Integer.toString(size));
+		int[] dp = new int[size+1];
+		if(size < 3) {
+			if (size == 1) System.out.println(1);
+			else System.out.println(2);
 		} else {
-			int[] dp = new int[size + 1];
 			dp[1] = 1;
 			dp[2] = 2;
-			for (int i = 3; i <= size; i++) {
-				dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+			for(int i=3; i<=size; i++) {
+				dp[i] = (dp[i-2] + dp[i-1])%10007;
 			}
-			bw.write((dp[size])+"");
+			System.out.println(dp[size]);
 		}
-		bw.flush();
-
 	}
 }
