307833d5908ec374bd97c53e47b71d33ff2a188f
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-04-14 23:45:57 +0900

    [level 3] Title: 아이템 줍기, Time: 0.41 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -3,28 +3,26 @@ import java.util.*;
 class Solution {
     static int[][] map = new int[101][101];
     static int compareCnt = Integer.MAX_VALUE;
-    static boolean visited[][] = new boolean[101][101];
+    // static boolean visited[][] = new boolean[101][101];
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         
-        for(int i=0; i< rectangle.length; i++) 
-        {
+        for(int i=0; i< rectangle.length; i++) {
             int lx = 2*rectangle[i][0];
             int ly = 2*rectangle[i][1];
             int rx = 2*rectangle[i][2];
             int ry = 2*rectangle[i][3];
-            for(int y=ly; y<=ry; y++)
-            {
-                for(int x=lx; x<=rx; x++)
-                {
-                	if(map[y][x] == 1 ) continue; 
+            for(int y=ly; y<=ry; y++) {
+                for(int x= lx; x<=rx; x++) {
+                    if(map[y][x] == 1) {
+                        continue;
+                    }
                     map[y][x] = 1;
                     
-                    if(x==lx || x==rx || y==ly || y ==ry)
-                    {
-                    	map[y][x] = 2;
+                    if(y == ry || y==ly || x==lx || x== rx) {
+                        map[y][x] = 2;
                     }
-                }
+                }    
             }
         }
         
@@ -38,35 +36,27 @@ class Solution {
         int[] moveY = {-1, 1, 0, 0};
         int[] moveX = {0, 0, -1, 1};
         boolean visited[][] = new boolean[101][101];
-        visited[y][x] = true;
-        Queue<Integer[]> queue = new LinkedList<>();
-        queue.offer(new Integer[]{y,x,cnt});
-        
-    
-        while(!queue.isEmpty())
-        {	
-        	Integer[] cur = queue.poll();
-        	int curY = cur[0];
-        	int curX = cur[1];
-        	int curCnt = cur[2];
-        	
-        	if(curY == itemY && curX == itemX)
-        	{
-        		compareCnt = Math.min(compareCnt, curCnt);
-        		return;
-        	}
-        	
-        	for(int i = 0; i<4; i++)
-            {
-                int newX = curX+moveX[i];
-                int newY = curY+moveY[i];
-
-                if(newX >= 0 && newY>=0 && newX<=100 && newY<=100 && map[newY][newX] == 2 && !visited[newY][newX])
-                {	
-                	visited[newY][newX] = true;
-                	queue.offer(new Integer[]{newY,newX, curCnt + 1});
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{y,x,cnt});
+        visited[y][x] =true;
+        while(!queue.isEmpty()) {	
+            int[] cur = queue.poll();
+            
+            if(cur[0] == itemY && cur[1] == itemX) {
+                compareCnt = Math.min(compareCnt, cur[2]);
+                continue;
+            }
+            
+            for(int i=0; i<4; i++) {
+                int newY = cur[0] + moveY[i];   
+                int newX = cur[1] + moveX[i];
+                if(newY>=0 && newY <101&& newX>=0 &&newX<101) {
+                    if(map[newY][newX] == 2 && !visited[newY][newX]) {
+                        visited[newY][newX] =true;
+                        queue.offer(new int[] {newY, newX, cur[2]+1});
+                    }
                 }
-            }	
+            }
 	    }
     }
 }
