ffaab70998f2736c19aa08bc87b1111cb70b6dcc
HwaSoo Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-05-09 15:39:30 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,34 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int n = Integer.parseInt(br.readLine());
-		
-		long[] array = new long[n+1];
-		if(n == 1) {
-			System.out.println("0");
-			return;
-		} else if (n ==2 || n==3){
-			System.out.println("1");
-			return;
-		}
-		
-		array[0] = 0;
-		array[1] = 0;
-		array[2] = 1;
-		array[3] = 1;
-		
-		for(int i=4; i<=n; i++) {
-			if(i%6==0) array[i] = Math.min(Math.min(array[i/3], array[i/2]), array[i-1]) + 1;
-			else if(i%3==0) array[i] = Math.min(array[i/3], array[i-1])+1;
-			else if(i%2==0) array[i] = Math.min(array[i/2], array[i-1])+1;
-			else array[i] = array[i-1]+1;
-		}
-		bw.write(array[n]+"");
-		bw.flush();
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        for(int i=2; i<=n; i++) {
+        	dp[i] = dp[i-1] + 1;
+        	if(i%3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+        	if(i%2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+        }
+        System.out.println(dp[n]);
 	}
+	
+	//top down (재귀)
+	/*
+	public static int d[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+        d = new int[number+1];
+        System.out.println(calculate(number));
+    }
+
+    public static int calculate(int number){
+    	System.out.println("calulate 함수: " + number);
+        if (number == 1){
+        	System.out.println("number == 1");
+            return 0;
+        }
+        if (d[number] > 0){
+        	System.out.println("d[number] > 0");
+            return d[number];
+        }
+        d[number] = calculate(number-1) + 1;
+        if (number%3 == 0) {
+            d[number] = Math.min(d[number],calculate(number/3) +1);
+        }
+        if (number%2 == 0) {
+            d[number] = Math.min(d[number],calculate(number/2) +1);
+        }
+        return d[number];
+    
+	}*/
 }
+
