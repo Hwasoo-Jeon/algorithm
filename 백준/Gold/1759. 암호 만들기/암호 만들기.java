d7731e6502b86a4cebda8bde3be43dc73670de03
HwaSoo Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-05-13 14:26:47 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,85 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	private static int L;
-	private static ArrayList<String> vList = new ArrayList<String>();
-	private static ArrayList<String> cList = new ArrayList<String>();
-	private static ArrayList<String> apList = new ArrayList<String>();
-	private static ArrayList<String> result = new ArrayList<String>();;
-	
+	private static int L, C;
+	private static char[] apArr, pwd;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		L = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		apArr = new char[C];
+		pwd = new char[L];
 		st = new StringTokenizer(br.readLine());
 		
 		for(int i=0; i<C; i++) {
-			String alphabet = st.nextToken();
-			apList.add(alphabet);
-			if(check(alphabet)) vList.add(alphabet);
-			else cList.add(alphabet);
+			apArr[i] = st.nextToken().charAt(0);
 		}
-		Collections.sort(apList);
-		Collections.sort(vList);
-		Collections.sort(cList);
-		// 모든 경우의 수 조합, 체크
-		boolean[] visited = new boolean[C];
-		combi("", visited, 0);
-	
-		Collections.sort(result);
-		for(String s : result) {
-			bw.write(s+"\n");
-		}
-		bw.flush();
+		Arrays.sort(apArr);
+		combi(0, 0);
 	}
-	private static boolean check(String alphabet) {
-		switch (alphabet) {
-		case "a":
-			return true;
-		case "e" :
-			return true;
-		case "i" :
-			return true;
-		case "o" :
-			return true;
-		case "u" :
-			return true;
-		default :
-			return false;	
-		}
-	}
-	private static boolean resultCheck(String code) {
+
+	private static boolean resultCheck() {
 		int consonantNum = 0;
-		boolean flag = false;
-		int n = code.length();
-		for(int i=0; i<n; i++) {
-			String s = code.substring(i, i+1);
-			if(vList.contains(s)) {
-				flag = true;
-			} else if(cList.contains(s)) {
-				consonantNum++;
-			}
+		int vowelNum = 0;
+		for(char c : pwd) {
+			if(c == 'a' || c =='e' || c== 'i' || c=='o' || c=='u') vowelNum++;
+			else consonantNum++;
 		}
-		if(flag && consonantNum>=2) return true; 
-		return false;
+		if(consonantNum >=2 && vowelNum >=1 ) return true;
+		else return false;
 	}
 	
-	private static void combi(String str, boolean[] visited, int depth) {
-		if(str.length() == L) {
-			if(resultCheck(str)) result.add(str);
+	private static void combi (int start, int depth) throws IOException {
+		if(depth == L) {
+			if(resultCheck()) {
+				System.out.println(pwd);
+			}
 			return;
 		}
-		for(int i=depth; i<apList.size(); i++) {
-			if(!visited[i]) {
-				if(depth!=0 && str.charAt(depth-1) > apList.get(i).charAt(0)) {
-					continue;
-				}
-				visited[i] = true;
-				combi(str+apList.get(i), visited, depth+1);
-				visited[i] = false;
-			}
+		for(int i=start; i<C; i++) {
+			pwd[depth] = apArr[i];
+			combi(i+1, depth+1);
 		}
 	}
 }
