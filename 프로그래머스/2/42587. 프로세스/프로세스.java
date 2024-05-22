351ea184b4bc48700cd5e0952ad75d3940c814f2
HwaSoo Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-05-22 16:30:42 +0900

    Update 프로세스.java

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -4,8 +4,6 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int cnt = priorities.length;
         Queue<Integer> lQ = new LinkedList<>();
-        Queue<Integer> rQ = new LinkedList<>();
-
         int[] arr = new int[cnt];
         for(int i=0; i<cnt; i++) {
             lQ.offer(i);            
@@ -26,29 +24,6 @@ class Solution {
             }
             answer++;
         }
-        
-        return cnt;
-        // for(int i : arr) {
-        //     System.out.println(i);
-        // }
-        // return arr[location];
+        return -1;
     }
 }
-
-/*
-int maxIdx = q.poll();
-            int innerSize = q.size();  
-            for(int i = 0; i < innerSize; i++) {
-                int nextIdx = q.poll();
-                if(priorities[maxIdx] < priorities[nextIdx]) {
-                    q.offer(maxIdx);
-                    maxIdx = i;
-                    q.poll();
-                } else {
-                  q.offer(nextIdx);  
-                }
-            }
-            arr[maxIdx] = answer;
-            answer++;
-
-*/
