c6a0b57c30abb43be15ca570c2bb076408017f14
HwaSoo Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-05-22 12:43:04 +0900

    [level 2] Title: 전화번호 목록, Time: 296.70 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,26 +3,14 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         
-        // 공백제거
-        // for(int i=0; i< phone_book.length; i++) {
-        //     phone_book[i].replace(" ","");
-        // }
-        
-        //사전순 정렬(유사 접두사)
         Arrays.sort(phone_book);
-        
-        //1개 요소를 가지고 나머지 요소 탐색, matches, 최대 1억
-        for(int i=0; i< phone_book.length -1; i++) {
-            int p_length = phone_book[i].length();
-            String p_str = phone_book[i];
-            int c_length = phone_book[i+1].length();
-            String c_str = phone_book[i+1];
-            if(p_str.charAt(0) != c_str.charAt(0) || c_length < p_length) {
-                continue;
-            } else if (c_str.startsWith(p_str)) {
+        int cnt = phone_book.length;
+        for(int i=0; i<cnt-1; i++) {
+            String preWord = phone_book[i];
+            String cur = phone_book[i+1]; //다음 문자와 비교만해도 판별 가능
+            if(cur.startsWith(preWord)) {
                 return false;
             }
-            
         }
         
         return true;
