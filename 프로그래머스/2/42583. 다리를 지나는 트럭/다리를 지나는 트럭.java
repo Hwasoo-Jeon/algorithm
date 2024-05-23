57c9751debfab32bab2950525ec8de2b2b7716b9
HwaSoo Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-05-23 14:11:25 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.51 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,53 +1,44 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> q = new LinkedList<>(); //건널트럭(입력 큐)
-        Queue<Integer> isCrossing = new LinkedList<>(); //건너는 트럭
-        Queue<Integer> isCrossingTime = new LinkedList<>(); //건너는 트럭의 시간
-        
-        for(int i=0; i<truck_weights.length; i++) {
-            q.offer(truck_weights[i]);
-        }
         
         int answer = 0;
         int bridgeWeight = 0;
-        int isCrossingCnt = 0;
-        int totalCnt = 0;
-        while(totalCnt != truck_weights.length) {
-            answer++;
-            if(!isCrossingTime.isEmpty() 
-               && isCrossingTime.peek() == bridge_length) {
-                isCrossingTime.poll();
-                bridgeWeight -= isCrossing.poll();
-                isCrossingCnt --;
-                totalCnt++;
-            }
+        Queue<int[]> bridgeQ = new LinkedList<>();
+        Queue<int[]> waitQ = new LinkedList<>();
+        
+        for(int i : truck_weights) {
+            waitQ.offer(new int[] {i, bridge_length});
+        }
+        
+        while(!waitQ.isEmpty() || !bridgeQ.isEmpty()) {
             
-            if(!q.isEmpty()) {
-                int next = q.peek();
-                if(bridgeWeight + next <= weight && isCrossingCnt < bridge_length) {
-                    bridgeWeight += next;
-                    isCrossingCnt++;
-                    isCrossing.offer(q.poll());
-                    isCrossingTime.offer(0);
+            //다리 위 트럭 이동
+            int cnt = bridgeQ.size();
+            for(int i=0; i< cnt; i++) {
+                int[] truck = bridgeQ.poll();
+                truck[1]--;
+                if(truck[1] > 0) {
+                    bridgeQ.offer(truck);
+                } else {
+                    bridgeWeight -= truck[0];
                 }
             }
             
-            for(int i=0; i<isCrossingTime.size(); i++) {
-                isCrossingTime.add(isCrossingTime.poll()+1);
+            
+            //대기 중 트럭을 다리에 진입
+            if(!waitQ.isEmpty()) {
+                int[] waitingTruck = waitQ.peek();
+                if (bridgeWeight + waitingTruck[0] <= weight) {
+                    bridgeWeight += waitingTruck[0];
+                    bridgeQ.offer(waitQ.poll());
+                }
             }
+            
+            //시간 증가
+            answer++;
         }
-
         return answer;
     }
 }
-/*
-모든 트럭 다리 건너는 시간 최소 몇초?
-정해진 순.
-bridge_length대
-weight 이하까지의 무게
-
-
-*/
