cf8470fb5e4cfe575c78d7195287e8ed74183475
HwaSoo Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-06-04 14:19:31 +0900

    [level 3] Title: 등굣길, Time: 0.85 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,38 +1,27 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n+1][m+1];
-        //dp이자 map
+        int[][] dp = new int[n+1][m+1]; //dp이자 map
+        
+        //물웅덩이 표기 
         for(int[] arr : puddles) {
-            int x = arr[0];
-            int y = arr[1];
-            dp[y][x] = -1;
+            dp[arr[1]][arr[0]] = -1;
         }
         
-        // 가장자리(우향, 하향) 초기화
-        boolean doNotEnter = false;
-        for(int y=1; y<=n; y++) {
-            if(dp[y][1] != 0) doNotEnter = true;
-            if(doNotEnter) dp[y][1] = -1;    
-            else dp[y][1] = 1; 
-        }    
-        
-        doNotEnter = false;
-        for(int x=2; x<=m; x++) {
-            if(dp[1][x] != 0) doNotEnter = true;
-            if(doNotEnter) dp[1][x] = -1; 
-            else dp[1][x] = 1;   
-        }
+        //집표시
+        dp[1][1] = 1;
         
         // 탐색
         for(int y=1; y<=n; y++) {
             for(int x=1; x<=m; x++) {
-                if(dp[y][x] == -1) continue;
-                if(y == 1 || x == 1) continue;
-                int left = dp[y][x-1] != -1 ? dp[y][x-1] : 0;
-                int up = dp[y-1][x] != -1 ? dp[y-1][x] : 0;
-                
-                dp[y][x] = (left + up) % 1000000007;
-                //System.out.println(y+" : "+x+" >>> "+dp[y][x]);
+                if(dp[y][x] == -1) {
+                    dp[y][x] = 0;
+                    continue;
+                }
+                //하향 움직임
+                if(y > 1) dp[y][x] += dp[y-1][x] % 1000000007;    
+                //우향 움직임
+                if(x > 1) dp[y][x] += dp[y][x-1] % 1000000007;
+                dp[y][x] %= 1000000007;
             }
         }
         
